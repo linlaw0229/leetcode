2bb2326ff8c31ee4e82e364b76a229d1182713a8
linlaw0229 <linlaw0229@gmail.com>
2018-01-23 16:33:29 -0600

    53

---
+++ 53. Maximum Subarray/solution.c
@@ -0,0 +1,19 @@
+/*
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum= 0, max= nums[0];
+        for(int i = 0; i< nums.size(); i++){
+            sum = nums[i] + (sum < 0 ? 0:sum);
+            max = sum > max ? sum:max;
+        }
+        return max;
+    }
+};
+
+
