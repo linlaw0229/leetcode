7e3323fc840c179369cd7c30fdf609be0f2f8a46
linlaw0229 <linlaw0229@gmail.com>
2018-02-03 17:14:48 -0600

    157

---
+++ 157. Read N Characters Given Read4/solution.c
@@ -0,0 +1,33 @@
+/*
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+
+Note:
+The read function will only be called once for each test case.
+*/
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+        int read= 0;
+        for(int i= 0; i<= n/4; i++){
+            int cur = read4(buf+read);
+            if(!cur)
+                break;
+            read += cur;
+        }
+        return min(read, n);
+        
+    }
+};
