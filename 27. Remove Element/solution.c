ef6d535187f4ce049173b80c7329cfba7d02e4b7
linlaw0229 <linlaw0229@gmail.com>
2018-03-24 13:42:36 -0500

    27

---
+++ 27. Remove Element/solution.c
@@ -0,0 +1,29 @@
+/*
+Given an array and a value, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+*/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count=0;
+        
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] == val)
+                ++count;
+            else
+                nums[i-count] = nums[i];
+            
+        }
+        return nums.size()-count;
+    }
+};
