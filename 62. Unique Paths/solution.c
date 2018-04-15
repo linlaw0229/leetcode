12f5704296f1d1e21d5cdd47c50a7d4ec2310836
linlaw0229 <linlaw0229@gmail.com>
2018-04-14 22:48:40 -0500

    62

---
+++ 62. Unique Paths/solution.c
@@ -0,0 +1,36 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+
+Above is a 3 x 7 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (m > n) return uniquePaths(n, m);
+        vector<int> cur(m, 1);
+        for (int j = 1; j < n; j++)
+            for (int i = 1; i < m; i++)
+                cur[i] += cur[i - 1]; 
+        return cur[m - 1];
+    }
+};
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int> > path(m, vector<int> (n, 1));
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                path[i][j] = path[i - 1][j] + path[i][j - 1];
+        return path[m - 1][n - 1];
+    }
+};
