c21752fd5ca3594fa5d5dfdfc652d15b2d90e46d
linlaw0229 <linlaw0229@gmail.com>
2018-05-31 11:19:01 -0500

    287

---
+++ 287. Find the Duplicate Number/solution2.c
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size()-1;
+        int low=1;
+        int high=n;
+        int mid;
+        while(low<high){
+            mid=(low+high)/2;
+            
+            int count=0;
+            for(int num:nums){
+                if(num<=mid) count++;
+            }
+            printf("low=%d, high=%d, mid=%d, count=%d\n", low, high, mid, count);
+            if(count>mid) high=mid;
+            else low=mid+1; 
+        }
+        return low;
+    }
+};
