749de262cc126aac7d878cc6dd9783ab1bb13d3b
linlaw0229 <linlaw0229@gmail.com>
2018-06-06 14:07:35 -0500

    159

---
+++ 159. Longest Substring with At Most Two Distinct Characters/solution.c
@@ -0,0 +1,44 @@
+/*
+Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
+
+Example 1:
+
+Input: "eceba"
+Output: 3
+Explanation: t is "ece" which its length is 3.
+Example 2:
+
+Input: "ccaabbb"
+Output: 5
+Explanation: t is "aabbb" which its length is 5.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int res = 0, left = 0;
+        unordered_map<char, int> m;
+        for (int i = 0; i < s.size(); ++i) {
+            ++m[s[i]];
+            while (m.size() > 2) {
+                if (--m[s[left]] == 0) m.erase(s[left]);
+                ++left;
+            }
+            res = max(res, i - left + 1);
+        }
+        return res;
+        /*
+        int left = 0, right = -1, res = 0;
+        for (int i = 1; i < s.length(); ++i) {
+            if (s[i] == s[i - 1]) continue; //current position character is the same as previous one
+            if (right >= 0 && s[right] != s[i]) {
+                res = max(res, i - left);
+                left = right + 1;
+            }
+            right = i - 1;
+            //1. first time would be have two different character
+            //2. after that, j means: when see different character, then move the end pointer j to previous substring
+        }
+        return max(int(s.length()) - left, res);*/
+    }
+};
