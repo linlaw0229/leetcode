753c07f7a24746df0c0d5dc6e29bd62b34c32fa6
linlaw0229 <linlaw0229@gmail.com>
2018-06-18 13:03:00 -0500

    151

---
+++ 151. Reverse Words in a String/solution.c
@@ -0,0 +1,35 @@
+/*
+Given an input string, reverse the string word by word.
+
+Example:  
+
+Input: "the sky is blue",
+Output: "blue is sky the".
+Note:
+
+A word is defined as a sequence of non-space characters.
+Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+You need to reduce multiple spaces between two words to a single space in the reversed string.
+Follow up: For C programmers, try to solve it in-place in O(1) space.
+
+
+*/
+
+class Solution {
+public:
+    void reverseWords(string &s) {
+        int storeIndex = 0, n = s.size();
+        reverse(s.begin(), s.end());
+        for (int i = 0; i < n; ++i) {
+            if (s[i] != ' ') {
+                if (storeIndex != 0) 
+                    s[storeIndex++] = ' '; //original place of storeIndex set to space, then move to next
+                int j = i;
+                while (j < n && s[j] != ' ') s[storeIndex++] = s[j++];
+                reverse(s.begin() + storeIndex - (j - i), s.begin() + storeIndex);
+                i = j;
+            }
+        }
+        s.resize(storeIndex);
+    }
+};
