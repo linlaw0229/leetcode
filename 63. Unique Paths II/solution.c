15126fda1182fb94999fe38065ad161a7f43178a
linlaw0229 <linlaw0229@gmail.com>
2018-06-24 17:18:12 -0500

    63

---
+++ 63. Unique Paths II/solution.c
@@ -0,0 +1,47 @@
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+
+*/
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid.empty() || obstacleGrid[0].empty() || obstacleGrid[0][0] == 1) return 0;
+        vector<vector<int> > dp(obstacleGrid.size(), vector<int>(obstacleGrid[0].size(), 0));
+        for (int i = 0; i < obstacleGrid.size(); ++i) {
+            for (int j = 0; j < obstacleGrid[i].size(); ++j) {
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if (i == 0 && j == 0) dp[i][j] = 1;
+                else if (i == 0 && j > 0) dp[i][j] = dp[i][j - 1];
+                else if (i > 0 && j == 0) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp.back().back();
+    }
+};
