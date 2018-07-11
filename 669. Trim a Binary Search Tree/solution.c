a0eafe78b71bba441e58ae8ad209206252aeff1d
linlaw0229 <linlaw0229@gmail.com>
2018-07-11 14:40:32 -0500

    669

--- 669. Trim a Binary Search Tree/solution.c
+++ 669. Trim a Binary Search Tree/solution.c
@@ -48,12 +48,11 @@ class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int L, int R) {
         if(!root) return root;
-        if(root->val < L) return trimBST(root->right, L, R);
-        if(root->val > R) return trimBST(root->left, L, R);
+        if(root->val < L) return trimBST(root->right, L, R); //there might be some elements in right subtree fit the requirement
+        if(root->val > R) return trimBST(root->left, L, R); //there might be some elements in left subtree fit the requirement
         
         root->left= trimBST(root->left, L, R);
         root->right= trimBST(root->right, L, R);
         return root;
     }
-    
 };
