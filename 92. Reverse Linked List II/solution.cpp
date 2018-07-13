c0ee1968c9477dbfea54afb6d712e042341c69dd
linlaw0229 <linlaw0229@gmail.com>
2018-07-13 16:16:27 -0500

    92'

---
+++ 92. Reverse Linked List II/solution.cpp
@@ -0,0 +1,46 @@
+/*
+Reverse a linked list from position m to n. Do it in one-pass.
+
+Note: 1 ≤ m ≤ n ≤ length of list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        ListNode* dummy= new ListNode(-1);
+        dummy->next= head;
+        ListNode *pre= dummy;
+        
+        //find the element that before left boundary
+        for(int i= 1; i< m; i++) pre= pre->next;
+        //pre= cur; //pre is the element before left boundary
+        ListNode* start= pre->next;
+        ListNode* then= start->next;
+        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+        
+        for(int i= m; i< n; i++){
+            start->next = then->next;
+            then->next = pre->next;
+            pre->next = then;
+            then = start->next;
+        }
+        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+        
+        return dummy->next;
+    }
+};
