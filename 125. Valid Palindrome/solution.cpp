3a7f51ff98c9e8271074f20c87b4445813b40eb4
linlaw0229 <linlaw0229@gmail.com>
2018-07-16 15:24:31 -0500

    125

---
+++ 125. Valid Palindrome/solution.cpp
@@ -0,0 +1,31 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+*/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start= 0, end= s.length()-1;
+        while(start< end){
+            if(!isalnum(s[start])) start++;
+            else if(!isalnum(s[end])) end--;
+            else{
+                if(tolower(s[start]) != tolower(s[end])) return false;
+                else
+                    start++, end--;
+            }
+        }
+        return true;
+    }
+};
