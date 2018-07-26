bb50b074ec30028baffb1acd657e9ae38d523808
linlaw0229 <linlaw0229@gmail.com>
2018-07-26 17:50:02 -0500

    350

---
+++ 350. Intersection of Two Arrays II/solution.cpp
@@ -0,0 +1,30 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+Note:
+Each element in the result should appear as many times as it shows in both arrays.
+The result can be in any order.
+Follow up:
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+*/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> dict;
+        vector<int> res;
+        for(int i = 0; i < (int)nums1.size(); i++) 
+            dict[nums1[i]]++;
+        for(int i = 0; i < (int)nums2.size(); i++)
+            if(dict.find(nums2[i]) != dict.end() && --dict[nums2[i]] >= 0) 
+                res.push_back(nums2[i]);
+        
+        return res;
+    }
+};
