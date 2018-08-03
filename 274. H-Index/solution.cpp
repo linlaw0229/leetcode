b40e69b3ddb919d295e4679f85359b7d0d0643d7
linlaw0229 <linlaw0229@gmail.com>
2018-08-03 14:31:31 -0500

    274

---
+++ 274. H-Index/solution.cpp
@@ -0,0 +1,36 @@
+/*
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+Example:
+
+Input: citations = [3,0,6,1,5]
+Output: 3 
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had 
+             received 3, 0, 6, 1, 5 citations respectively. 
+             Since the researcher has 3 papers with at least 3 citations each and the remaining 
+             two with no more than 3 citations each, her h-index is 3.
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+*/
+//time complexity: O(n)
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n= citations.size();
+        vector<int> check(n+1, 0);
+        for(int i = 0; i< n; i++){
+            if(citations[i] > n)
+                check[n]++;
+            else
+                check[citations[i]]++;
+        }
+        
+        int sum= 0;
+        for(int i= n; i>= 0; i--){
+            sum+= check[i];
+            if(sum >= i)
+                return i;
+        }   
+    }
+};
