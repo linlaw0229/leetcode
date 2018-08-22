003268a1bc0a252c9a21cf5ed1b4913f27923c20
linlaw0229 <linlaw0229@gmail.com>
2018-08-22 14:49:26 -0500

    247

---
+++ 247. Strobogrammatic Number II/solution.cpp
@@ -0,0 +1,30 @@
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Find all strobogrammatic numbers that are of length = n.
+
+Example:
+
+Input:  n = 2
+Output: ["11","69","88","96"]
+*/
+
+class Solution {
+public:    
+    vector<string> findStrobogrammatic(int n) {
+        return find(n, n);
+    }
+    vector<string> find(int m, int n) { //m: which layer currently at. n: which layer need to reach
+        if (m == 0) return {""};
+        if (m == 1) return {"0", "1", "8"};
+        vector<string> t = find(m - 2, n), res;
+        for (auto a : t) {
+            if (m != n) res.push_back("0" + a + "0"); //0 and 0 can not be in the front and end
+            res.push_back("1" + a + "1");
+            res.push_back("6" + a + "9");
+            res.push_back("8" + a + "8");
+            res.push_back("9" + a + "6");
+        }
+        return res;
+    }
+};
